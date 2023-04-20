63877a28b882ac89b51a046c829229b0e315b112
Derkysan <derkysan19@gmail.com>
2023-04-20 16:58:42 -0400

    animation

--- src/website/pages/HomePage.tsx
+++ src/website/pages/HomePage.tsx
@@ -1,12 +1,34 @@
-import { Link } from 'react-router-dom';
 import Typed from 'typed.js';
+import logo from "../../../assets/blanco.png";
+import { useEffect, useRef } from 'react';
 
 export const HomePage = () => {
+
+  const el = useRef(null);
+  const typed = useRef<any>(null);
+
+  useEffect(() => {
+    const options = {
+      strings: ['Fusionamos tecnología y creatividad para transformar el mundo'],
+      typeSpeed: 25,
+      backSpeed: 50,
+      cursorChar: '_',
+      startDelay: 750,
+    };
+    
+    typed.current = new Typed(el.current, options);
+    
+    return () => {
+      // Make sure to destroy Typed instance during cleanup
+      // to prevent memory leaks
+      typed.current.destroy();
+    }
+  }, [])
+
   return (
     <div className="main bg-black text-white animate__animated animate__fadeIn">
 
       <div className="container h-100">
-        {/* <div className="row h-100 align-items-center justify-content-center"> */}
         <div className="row h-100">
           <div className="col h-100">
             
@@ -14,14 +36,17 @@ export const HomePage = () => {
               <div></div>
 
               <div>
-                <img src="./assets/images/blanco.png" width="80" className="mb-3" alt="Blanco" />
-                <h1 className="display-2 fw-light">Blanco - Brand</h1>
-                <p style={{ letterSpacing: '.4em', fontSize: '12px' }} className="fw-light">Fusionamos tecnología y creatividad para transformar el mundo</p>
-                <hr style={{ width: '150px', margin: '3em auto 2em' }} />
-                <div className="d-flex justify-content-center">
-                  <Link className="btn btn-circle btn-lg text-secondary" to={'/'}>
+                <img src={logo} width="80" className="mb-3" alt="Blanco" />
+                <h1 className="display-3 fw-light text-uppercase ">Blanco - Brand</h1>
+                <span style={{ letterSpacing: '.4em', fontSize: '12px' }} className="fw-light" ref={el}></span>
+                <hr style={{ width: '100px', margin: '3em auto 2em', borderWidth: '2px' }} />
+                <div className="d-flex justify-content-center align-items-center">
+                  <a href="mailto:info@blanco-brand.com" className="btn btn-circle btn-lg">
                     <i className="bi bi-envelope"></i>
-                  </Link>
+                  </a>
+                  <a href="https://www.linkedin.com/company/estudio-blanco/" target="_blank" className="btn btn-circle btn-lg">
+                    <i className="bi bi-linkedin"></i>
+                  </a>
                 </div>
               </div>
 
